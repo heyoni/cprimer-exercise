19072f81ac6430d84ea9341c118aa440471ed379
Jonathan Revah <jonathanrevah@yoni.local>
2013-06-13 15:04:21 -0400

    Initial Commit

---
+++ cprimer exercise/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cprimer exercise
+//
+//  Created by Jonathan Revah on 06/13/2013.
+//  Copyright (c) 2013 Jonathan Revah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
